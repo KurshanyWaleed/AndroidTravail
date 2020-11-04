61215c5fbd859089a010d2c0da7e99f59d186db2
kurshanyWaleed <kurshany.waleed@gmail.com>
2020-11-04 20:15:02 +0100

    1.0

---
+++ app/src/main/java/com/dmwm/myapplicationtd/Failed.java
@@ -0,0 +1,34 @@
+package com.dmwm.myapplicationtd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Failed extends AppCompatActivity {
+    Button sms;
+    TextView txt;
+    String str="Désole vous n'avez pas la moyenne ";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_failed);
+        sms=findViewById(R.id.buttonSMS);
+        Intent i = getIntent();
+        float moy=i.getFloatExtra("moyenne",0);
+        txt.setText(str+" "+moy);
+
+
+        sms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(str));
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/dmwm/myapplicationtd/MainActivity.java
@@ -0,0 +1,49 @@
+package com.dmwm.myapplicationtd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edt_txt_java,edt_txt_cs,edt_txt_python;
+    Button btn_apply;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edt_txt_cs=findViewById(R.id.edt_txt_cs);
+        edt_txt_java=findViewById(R.id.edt_txt_java);
+        edt_txt_python=findViewById(R.id.edt_txt_py);
+        btn_apply=findViewById(R.id.buttonAplly);
+
+
+
+        btn_apply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                float note_java = Float.parseFloat(String.valueOf(edt_txt_java.getText()));
+                float note_cs = Float.parseFloat(String.valueOf(edt_txt_cs.getText()));
+                float note_py = Float.parseFloat(String.valueOf(edt_txt_python.getText()));
+                float moyenne=(note_cs+note_java+note_py)/3;
+                if (moyenne >= 10.0) {
+                    Intent i = new Intent(getApplicationContext(),Succeeded.class );
+                    i.putExtra("Mayenne",moyenne);
+                    startActivity(i);
+
+                }
+                else
+                {
+                    Intent i = new Intent(getApplicationContext(),Failed.class );
+                    i.putExtra("Mayenne",moyenne);
+                    startActivity(i);
+                }
+            }
+        });
+    }
+
+}
